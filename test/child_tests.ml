43effb951c2acb99b10b05681bcba0d822ea03cd
Milo Turner <iitalics@gmail.com>
2017-10-23 00:01:38 -0400

    better substring-based testing

--- test/child_tests.ml
+++ test/child_tests.ml
@@ -1,126 +1,125 @@
 open Batteries
 open OUnit2
 
-(** monad for mock testing **)
-module Queue_mock = struct
-  exception Aborted of [ `quit
-                       | `sent of int * string
-                       ]
+module Mock = struct
+  exception PrematureQuit
 
-  type mock = {
-      con_id : int;
+  type logger = {
       mutable state : Child.state;
-      mutable queue :
-                [ `send of string
-                | `recv of int * string
-                | `quit ] list;
+      outputs : (int, Text.t) Hashtbl.t;
     }
 
-  type 'a t = mock -> 'a
-  let bind f g m = g (f m) m
-  let return = const
-  let map = (%)
+  module Monad = struct
+    type 'a t = logger -> 'a
+    let return = const
+    let bind f g lo = g (f lo) lo
+    let map g f lo = g (f lo)
 
-  let con_id m = m.con_id
-  let get_s m = m.state
-  let put_s s m = m.state <- s
+    let con_id _ = 0
+    let get_s lo = lo.state
+    let put_s s lo = lo.state <- s
 
-  let quit _ = raise (Aborted `quit)
+    let send i str lo =
+      let add_str s = Text.append s (Text.of_string str) in
+      Hashtbl.modify_def Text.empty i add_str lo.outputs
 
-  let send con str m =
-    match m.queue with
-    | (`recv (i, str'))::q' when con = i && str = str' ->
-       m.queue <- q'
-
-    | _ -> raise @@ Aborted (`sent (con, str))
-
+    let quit _ =
+      raise PrematureQuit
+  end
 end
-
-(** functon for running mocker with the givens state *)
-let mock m =
-  let module M = Queue_mock in
-  let module H = Child.Make(M) in
-  let open M in
-  let pp = function
-    | `send s -> Printf.sprintf "[client <- %S]" s
-    | `recv (c,s) -> Printf.sprintf "[%S -> #%d]" s c
-    | `quit -> "[quit]"
-  in
-  try
-    while true do
-      match m.queue with
-      | (`send str)::q' ->
-         m.queue <- q';
-         H.recv str m
-
-      | _ -> raise (Aborted `quit)
-    done
-  with
-  | Aborted `quit ->
-     assert_equal ~printer:pp
-       (List.at_opt m.queue 0 |> Option.default `quit)
-       `quit
-
-  | Aborted (`sent (c, s)) ->
-     assert_equal ~printer:pp
-       (List.at_opt m.queue 0 |> Option.default `quit)
-       (`recv (c, s))
 
 
 (** run mock with presupplied queue **)
-let queue_mock ~queue:q =
-  let module M = Queue_mock in
+let run_mock actions ~expect:expected =
+  let module M = Mock.Monad in
   let module H = Child.Make(M) in
-  let open M in
-  mock { con_id = 0;
-         state = H.init ();
-         queue = q }
+  (* init *)
+  let outputs = Hashtbl.create 30 in
+  let lo = {
+      Mock.state = H.init ();
+      Mock.outputs = outputs }
+  in
+
+  (* run all actions *)
+  let remaining = ref (List.length actions) in
+  (try
+     actions
+     |> List.iter (fun a ->
+            remaining := !remaining - 1;
+            match a with
+            | `send s -> H.recv s lo)
+   with
+     Mock.PrematureQuit ->
+      if !remaining > 0 then
+        assert_failure "unexpected premature quit");
+
+  (* check for expected substrings *)
+  expected
+  |> List.iter (function
+         | `recv (i, substr) ->
+            let txt = Hashtbl.find_default outputs i Text.empty in
+            if Text.exists txt (Text.of_string substr) then
+              assert_bool "ok" true
+            else
+              (print_string "\nBEGIN client output:\n";
+               print_string (Text.to_string txt);
+               print_string "\nEND client output\n";
+               assert_bool
+                 (Printf.sprintf "missing substring %S in con #%d" substr i)
+                 false)
+
+         | `final_state s ->
+            assert_bool "final state not expected"
+              (s = lo.Mock.state)
+       )
+
+
 
 
 let quit_test _ = begin
-    queue_mock [`send "QUIT\r\n";
-                `recv (0, ":test.irc ERROR :Bye\r\n");
-                `quit ]
+    run_mock [`send "QUIT\r\n"]
+      ~expect:[`recv (0, "ERROR :Bye\r\n")];
   end
 
 
 let err_test_1 _ = begin
-    queue_mock [`send "FOO\r\n";
-                `recv (0, ":test.irc 421 * FOO :Unknown command\r\n") ];
+    run_mock
+      [`send "FOO\r\n"]
+      [`recv (0, "421 * FOO :Unknown command\r\n")];
 
+    run_mock
+      [`send "NICK milo\r\n";
+       `send "FOO\r\n"]
+      [`recv (0, "421 milo FOO :Unknown command\r\n")];
+
+  end
+
+let err_test_2 _ = begin
     let param_count cmd n =
-      let fmt = Printf.sprintf in
-      0 -- (n - 1)
-      |> Enum.iter (fun i ->
-             let args =
-               1 -- i
-               |> Enum.map (const " *")
-               |> Enum.fold (^) ""
-             in
-             queue_mock [`send (fmt "%s%s\r\n" cmd args);
-                         `recv (0, fmt ":test.irc 461 * %s :Not enough parameters\r\n" cmd) ]
-           )
+      let mk_args k = String.repeat " *" k in
+      for i = 1 to n do
+        run_mock [`send (cmd ^ mk_args (i - 1) ^ "\r\n")]
+          ~expect:[`recv (0, cmd ^ " :Not enough parameters\r\n")]
+      done
     in
 
     param_count "NICK" 1;
     param_count "USER" 4;
-
-    queue_mock [`send "NICK milo\r\n";
-                `send "FOO\r\n";
-                `recv (0, ":test.irc 421 milo FOO :Unknown command\r\n") ];
   end
 
-let err_test_2 _ = begin
-    queue_mock [`send "NICK 123\r\n";
-                `recv (0, ":test.irc 432 * 123 :Erroneous nickname\r\n") ];
+let err_test_3 _ = begin
+    run_mock
+      [`send "NICK @a\r\n"]
+      [`recv (0, "432 * @a :Erroneous nickname\r\n") ];
   end
 
 let motd_test _ = begin
-
-    queue_mock [`send "NICK milo\r\n";
-                `send "USER milo * * :Milo Turner\r\n";
-                `recv (0, ":test.irc 375 :- test.irc Message of the day -\r\n");
-                `recv (0, ":test.irc 372 :- Hello\r\n") ];
+    run_mock
+      [`send "NICK milo\r\n";
+       `send "USER milo * * :Milo Turner\r\n"]
+      [`recv (0, "375 :- test.irc Message of the day -\r\n");
+       `recv (0, "372 :- Hello\r\n");
+       `final_state (Child.LoggedIn "milo")];
 
   end
 
@@ -129,7 +128,8 @@ let tests =
   "test.child"
   >::: List.rev [
       "quit" >:: quit_test;
-      "err_1" >:: err_test_1;
-      "err_2" >:: err_test_2;
+      "err_1_unknown_cmd" >:: err_test_1;
+      "err_2_param_count" >:: err_test_2;
+      "err_3" >:: err_test_3;
       "motd" >:: motd_test;
     ]
