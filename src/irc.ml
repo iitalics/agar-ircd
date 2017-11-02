ef5459a392bfb35ffaca5104cff39d70bcb4720d
Milo Turner <iitalics@gmail.com>
2017-11-01 21:48:07 -0400

    some doc fixes

--- src/irc.ml
+++ src/irc.ml
@@ -2,8 +2,8 @@ open Batteries
 
 (**
     Common types, type aliases, and functions for
-    simple IRC primitives like messages and message prefixe
- **)
+    simple IRC primitives like messages and message prefixes.
+ *)
 
 type nick_name = string
 type user_name = string
@@ -11,31 +11,31 @@ type host_name = string
 
 type prefix = nick_name option * user_name option * host_name option
 
-(** messages prefixes **)
+(** messages prefixes *)
 module Prefix = struct
   type t = prefix
 
-  (** an empty prefix. **)
+  (** an empty prefix. *)
   let empty : t = None, None, None
-  (** is the given prefix empty? **)
+  (** is the given prefix empty? *)
   let is_empty = ( = ) empty
 
-  (** [of_host h] creates a prefix with just hostname [h]. **)
+  (** [of_host h] creates a prefix with just hostname [h]. *)
   let of_host hos = None, None, Some hos
-  (** [of_user_host u h] creates a prefix with username [u] and hostname [h]. **)
+  (** [of_user_host u h] creates a prefix with username [u] and hostname [h]. *)
   let of_user_host usr hos = None, Some usr, Some hos
   (** [of_triple n u h] creates a prefix with nickname [n],
-      username [u] and hostname [h]. **)
+      username [u] and hostname [h]. *)
   let of_triple nic usr hos = Some nic, Some usr, Some hos
 
-  (** prints a prefix as [<nick>![<user>@[<host>]]] **)
+  (** prints a prefix as [<nick>![<user>@[<host>]]] *)
   let print out (pre_nic, pre_usr, pre_hos) =
     let open Printf in
     pre_nic |> Option.may (fprintf out "%s!");
     pre_usr |> Option.may (fprintf out "%s@");
     pre_hos |> Option.may (fprintf out "%s")
 
-  (** [to_string pfx] converts prefix [pfx] to a string using [print] **)
+  (** [to_string pfx] converts prefix [pfx] to a string using [print] *)
   let to_string pfx =
     let out = IO.output_string () in
     print out pfx;
@@ -53,14 +53,14 @@ module Msg = struct
   type t = msg
 
   (** [simple c pms] creates a message with command [c] and
-      parameters [pms]. **)
+      parameters [pms]. *)
   let simple c pms =
     { prefix = Prefix.empty;
       command = c;
       params = pms }
 
   (** [simple1 c pm] creates a message with command [c] and a single
-      parameter [pm]. **)
+      parameter [pm]. *)
   let simple1 c pm =
     { prefix = Prefix.empty;
       command = c;
@@ -68,7 +68,7 @@ module Msg = struct
 
   (** [format c pre fmt ...] creates a message with command [c],
       parameters [pre] follow by a final parameter using the format
-      string [fmt] and the following arguments. **)
+      string [fmt] and the following arguments. *)
   let format c pre fmt =
     Printf.ksprintf (fun s ->
         simple c (pre @ [s]))
@@ -81,26 +81,23 @@ module Msg = struct
       parameter (the target of the reply), followed by parameters
       [pms]. the argument order is flipped around because this
       allows higher-order manipulation of replies without immediately
-      passing the nick. **)
+      passing the nick. *)
   let reply n pms nick =
     simple (string_of_int n) (nick :: pms)
 
-  (** combination of [reply] and [format]. **)
+  (** combination of [reply] and [format]. *)
   let replyf n pre fmt =
     Printf.ksprintf (fun s ->
         reply n (pre @ [s]))
       fmt
 
-  (** prints in a format that can be parsed back. **)
+  (** prints in a format that can be parsed back. *)
   let print out m =
     let open Printf in
     let rec write_args = function
       | [] -> ()
-      | [pm] ->
-         Printf.fprintf out " :%s" pm
-      | pm::rest ->
-         Printf.fprintf out " %s" pm;
-         write_args rest
+      | [pm] -> fprintf out " :%s" pm
+      | pm::rest -> fprintf out " %s" pm; write_args rest
     in
     if not (Prefix.is_empty m.prefix) then
       begin
@@ -111,7 +108,7 @@ module Msg = struct
     fprintf out "%s" m.command;
     write_args m.params
 
-  (** [to_string m] converts message [m] to a string using [print]. **)
+  (** [to_string m] converts message [m] to a string using [print]. *)
   let to_string m =
     let out = IO.output_string () in
     print out m;
