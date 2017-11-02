043ffc9b643141d3b94ad06c25d44d6efa1775d3
Milo Turner <iitalics@gmail.com>
2017-11-02 11:08:13 -0400

    beginning BatMap based database implementation

--- src/database.ml
+++ src/database.ml
@@ -1,34 +1,79 @@
 open Batteries
 
 type con = int
+module Con = Int
 
-
-type guest_entry = {
-    gent_con : con;
-    gent_nick : Irc.nick_name option;
-    gent_user : Irc.user_name option;
-    gent_real : Irc.real_name option;
-  }
-
-type guests = unit
-let create_guests ~size = ()
-
-
-type user_details = {
-    udet_username : Irc.user_name;
+type user_details =
+  { udet_username : Irc.user_name;
     udet_realname : Irc.real_name;
     udet_addr : Unix.sockaddr;
-    udet_oper : [`Dom | `Sub | `Van];
-  }
+    udet_oper : [`Dom | `Sub | `Van] }
 
 type host
-  = Host_there of Irc.nick_name
+  = Host_there of Irc.nick_name * con
   | Host_here of user_details
 
-type user_entry = {
-    uent_con : con;
-    uent_nick : string option;
-  }
+type guest_entry =
+  { gent_con : con;
+    gent_nick : Irc.nick_name option;
+    gent_user : Irc.user_name option;
+    gent_real : Irc.real_name option }
 
-type users = unit
-let create_users ~size = ()
+type user_entry =
+  { uent_con : con;
+    uent_nick : string;
+    uent_host : host }
+
+
+(**
+   Module for database of guests: users
+   who have recently connected and have not
+   picked a username and a nickname.
+ *)
+module Guests = struct
+  module M = Map.Make(Con)
+
+  type t = guest_entry M.t
+
+  let empty : t = M.empty
+
+  (** [by_con c guests] returns [Some ge], where [ge] is the
+      guest entry with connection id [c], or [None] if that
+      connection is not a guest. *)
+  let by_con c gs =
+    try
+      Some (M.find c gs)
+    with Not_found -> None
+
+end
+
+(**
+   Module for database of users how are
+   connected somewhere on the network. Note that
+   operators are users too.
+ *)
+module Users = struct
+  module StrM = Map.Make(String)
+  module ConM = Map.Make(Con)
+
+  type t = user_entry ConM.t * con StrM.t
+
+  let empty = ConM.empty, StrM.empty
+
+  (** [by_con c users] returns [Some ue], where [ue] is the
+      user entry with connection id [c], or [None] if that
+      connection is not a user. *)
+  let by_con c (us, _) =
+    try
+      Some (ConM.find us c)
+    with Not_found -> None
+
+  (** [by_nick nick users] returns [Some ue], where [ue] is the
+      user entry with nickname [nick], or [None] if that nick
+      does not exist. *)
+  let by_nick nic (us, ns) =
+    try
+      Some (ConM.find us (StrM.find ns nic))
+    with Not_found -> None
+
+end
