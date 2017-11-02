0409753022d67ede6a5d9bd4f37a216fa8ac238f
Milo Turner <iitalics@gmail.com>
2017-11-02 11:22:38 -0400

    actors have no internal state

--- src/actor.ml
+++ src/actor.ml
@@ -7,9 +7,9 @@ module DB = Database
 module type MONAD = sig
   include Monad.SIG
 
-  (* val get_parent_con : DB.con option t *)
+  val get_parent_con : DB.con option t
   val get_con : DB.con t
-  (* val send_msg : DB.con -> Irc.msg -> unit t *)
+  val send_msg : DB.con -> Irc.msg -> unit t
   (* val should_close : bool -> unit t *)
 
   (* val with_users : ?mut:bool -> (DB.users -> 'a) -> 'a t *)
@@ -23,11 +23,9 @@ end
 module type IMPL =
   functor(M : MONAD) -> sig
 
-    type st
-
-    val on_init : unit -> st M.t
-    val on_quit : st -> unit M.t
-    val on_recieve : st -> Irc.msg -> st M.t
+    val on_init : unit -> unit M.t
+    val on_quit : unit -> unit M.t
+    val on_recieve : Irc.msg -> unit M.t
 
   end
 
@@ -39,11 +37,8 @@ module Impl(M : MONAD) = struct
   module Ex = Monad.Extra(M)
   open Ex
 
-  type st
-    = Guest | User
-
-  let on_init () = M.pure Guest
-  let on_quit st = pure_
-  let on_recieve st _ = M.pure st
+  let on_init () = pure_
+  let on_quit () = pure_
+  let on_recieve _ = pure_
 
 end
